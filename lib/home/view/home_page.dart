89f4a91f951a951f2089f0f331d833655784a311
Keval Aamodra <patelkeval936@gmail.com>
2024-03-09 13:36:25 +0530

    added gradient in home page

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:stroll_social_assignment/core/extensions/build_context_extensions.dart';
+import 'package:stroll_social_assignment/home/widgets/sub_headers.dart';
 import 'package:stroll_social_assignment/utils/app_assets.dart';
+
+import '../models/user_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,7 +27,31 @@ class _HomePageState extends State<HomePage> {
               ),
               fit: BoxFit.cover),
         ),
-        child: Container(),
+        child: Stack(
+          children: [
+            Container(
+              height: context.screenHeight,
+              width: context.screenWidth,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  const Color(0xff000000).withOpacity(0),
+                  const Color(0xff000000).withOpacity(0.15),
+                  const Color(0xff000000).withOpacity(0.25),
+                  const Color(0xff000000).withOpacity(0.35),
+                  const Color(0xff000000).withOpacity(0.6),
+                  const Color(0xff000000).withOpacity(0.85),
+                  const Color(0xff000000).withOpacity(1),
+                  const Color(0xff000000).withOpacity(1),
+                  const Color(0xff000000).withOpacity(1),
+                ],
+                // stops: [0,28,64,80,100]
+              )),
+            ),
+          ],
+        ),
       ),
     );
   }

f9c88e36e058a6a68f9396513507a5bd7bb152df
Keval Aamodra <patelkeval936@gmail.com>
2024-03-10 02:48:04 +0530

    updated header with its models and data source

---
+++ lib/home/presentation/bloc/header_bloc/header_bloc_event.dart
@@ -0,0 +1,8 @@
+
+abstract class HeaderBlocEvents {
+  const HeaderBlocEvents();
+}
+
+class GetData extends HeaderBlocEvents {
+  const GetData();
+}
---
+++ lib/home/presentation/widgets/header.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:stroll_social_assignment/home/presentation/widgets/heading_subtitle.dart';
+import 'package:stroll_social_assignment/utils/app_styles.dart';
+
+import '../../../utils/app_assets.dart';
+import '../bloc/header_bloc/header_bloc.dart';
+import '../bloc/header_bloc/header_bloc_state.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return BlocBuilder<HeaderBloc, HeaderBlocState>(builder: (context, state) {
+
+      if (state is HeaderLoading) {
+        return const CircularProgressIndicator();
+      }
+
+      if (state is HeaderFailed) {
+        return Text(state.message,style: AppStyles.errorStyle);
+      }
+
+      if (state is HeaderSuccess && state.headerInfo != null) {
+        return Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  state.headerInfo?.title ?? '',
+                  style: AppStyles.headerTextStyle,
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Material(
+                  color: Colors.transparent,
+                  child: IconButton(
+                      splashRadius: 10,
+                      constraints: const BoxConstraints(maxWidth: 20,maxHeight: 20),
+                      padding: EdgeInsets.zero,
+                      onPressed: () {},
+                      icon: Padding(
+                        padding: const EdgeInsets.only(top: 6),
+                        child: Center(
+                            child: SvgPicture.asset(
+                          AppAssets.arrowDownIcon,
+                          width: 12,
+                        )),
+                      )),
+                )
+              ],
+            ),
+            const SizedBox(height: 2,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                HeadingSubtitle(
+                    leadingIcon: AppAssets.clockIcon,
+                    text: state.headerInfo?.time ?? ''),
+                const SizedBox(
+                  width: 10,
+                ),
+                HeadingSubtitle(
+                    leadingIcon: AppAssets.userSmallIcon,
+                    text: state.headerInfo?.followers.toString() ?? ''),
+              ],
+            )
+          ],
+        );
+      }
+
+      return Container();
+    });
+  }
+}

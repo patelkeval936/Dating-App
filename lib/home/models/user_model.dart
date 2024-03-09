e8dc7e63e1b3980e10d6f37f26c14ea2f8646ccc
Keval Aamodra <patelkeval936@gmail.com>
2024-03-10 02:59:40 +0530

    removed unnecessary files

--- lib/home/models/user_model.dart
+++
@@ -1,9 +0,0 @@
-class HeaderInfoModel {
-  String title;
-  String time;
-  int followers;
-
-  HeaderInfoModel(this.title, this.time, this.followers);
-}
-
-HeaderInfoModel user = HeaderInfoModel('Stroll Bonfire', "22h 00m", 103);

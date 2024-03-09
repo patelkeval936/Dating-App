0bb18869d081d4b94890f3d181fed68efd000a03
Keval Aamodra <patelkeval936@gmail.com>
2024-03-10 02:41:54 +0530

    updated app utils files of string, colors and theme

--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,5 +1,7 @@
 
 class AppAssets{
+
+  //icons & images
   static const bgImage = 'assets/images/bg.jpeg';
   static const profilePicImage = 'assets/images/profile_pic.jpeg';
   static const chatIcon = 'assets/icons/chat_icon.svg';
@@ -7,6 +9,13 @@ class AppAssets{
   static const userSmallIcon = 'assets/icons/user_small_icon.svg';
   static const clockIcon = 'assets/icons/clock_icon.svg';
   static const matchesIcon = 'assets/icons/matches_icon.svg';
+  static const micIcon = 'assets/icons/mic.svg';
   static const swipeCardsIcon = 'assets/icons/swipe_cards_icon.svg';
+  static const arrowDownIcon = 'assets/icons/arrow_down.svg';
+
+  //json
+  static const headerJson = 'assets/data/header_data.json';
+  static const questionJson = 'assets/data/question_data.json';
+  static const userJson = 'assets/data/user_data.json';
 
 }
---
+++ lib/utils/app_strings.dart
@@ -0,0 +1,12 @@
+
+class AppStrings{
+  static const appName = "Stroll Social";
+  static const pickYourOption = "Pick your Option.";
+  static const hasSimilarMind = "See who has similar mind.";
+  static const somethingWentWrong = "Something went wrong";
+
+  static const swipe = "Swipe";
+  static const match = "Match";
+  static const chat = "Chat";
+  static const profile = "Profile";
+}

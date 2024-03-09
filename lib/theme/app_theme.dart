2884f3ec4308ccd894983266d0fe44c52dab823f
Keval Aamodra <patelkeval936@gmail.com>
2024-03-10 02:41:54 +0530

    updated app utils files of string, colors and theme

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData get light {
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFF13B9FF),
+      ),
+    );
+  }
+
+  static ThemeData get dark {
+    return ThemeData(
+      useMaterial3: false,
+      colorScheme: ColorScheme.fromSeed(
+        brightness: Brightness.dark,
+        seedColor: const Color(0xff8B88EF),
+      ),
+    );
+  }
+}
---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+
+  static const errorColor = Colors.red;
+  static const blackColor = Colors.black;
+
+  static const primaryColor = Color(0xff8B88EF);
+  static const optionWhite = Color(0xffC4C4C4);
+  static const captionPurple = Color(0xffCBC9FF);
+  static const headerColor = Color(0xffCCC8FF);
+  static const lightShadowColor = Color(0xffBEBEBE);
+  static const darkShadowColor = Color(0xff24232f);
+  static const badgeBackgroundColor = Color(0xffB5B2FF);
+  static const lightShadow = Color(0x0000004D);
+  static const borderBlack = Color(0xff0F1115);
+  static const greyColor = Color(0xff232A2E);
+  static const darkGreyColor = Color(0xff121518);
+  static const navBarColor = Color(0xff0F1115);
+  static const greyShadowColor = Color(0x4848484D);
+
+}

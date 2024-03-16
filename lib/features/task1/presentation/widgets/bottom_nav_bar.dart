77b70bb9cea7c930dec66e0103373e7bfc3ce604
Keval Aamodra <patelkeval936@gmail.com>
2024-03-16 22:46:07 +0530

    created bottom-nav-bar a separate widget

--- lib/features/task1/presentation/widgets/bottom_nav_bar.dart
+++ lib/features/task1/presentation/widgets/bottom_nav_bar.dart
@@ -1,39 +1,58 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:stroll_social_assignment/utils/app_colors.dart';
 import 'package:stroll_social_assignment/utils/app_strings.dart';
 import '../../../../utils/app_assets.dart';
+import '../../../task3/presentation/bloc/bottom_nav/bottom_nav_cubit.dart';
 import 'bottom_navbar_item.dart';
 
-Widget get bottomNavigationBar => Container(
+class CustomBottomNavBar extends StatelessWidget {
+  const CustomBottomNavBar({super.key, this.showBackground = true});
+
+  final bool showBackground;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       height: Platform.isIOS ? 76 : 64,
-      color: AppColors.navBarColor,
+      color: showBackground ? AppColors.navBarColor : Colors.transparent,
       child: Padding(
         padding: EdgeInsets.only(bottom: Platform.isIOS ? 12 : 0),
         child: const Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             BottomNavBarItem(
-                icon: AppAssets.swipeCardsIcon,
-                label: AppStrings.swipe,
-                showBadge: false,
-                badgeValue: ''),
+              icon: AppAssets.swipeCardsIcon,
+              label: AppStrings.swipe,
+              showBadge: false,
+              badgeValue: '',
+              index: 0,
+            ),
             BottomNavBarItem(
-                icon: AppAssets.matchesIcon,
-                label: AppStrings.match,
-                showBadge: true,
-                badgeValue: ''),
+              icon: AppAssets.matchesIcon,
+              label: AppStrings.match,
+              showBadge: false,
+              badgeValue: '',
+              index: 1,
+            ),
             BottomNavBarItem(
-                icon: AppAssets.chatIcon,
-                label: AppStrings.chat,
-                showBadge: true,
-                badgeValue: '10'),
+              icon: AppAssets.chatIcon,
+              label: AppStrings.chat,
+              showBadge: true,
+              badgeValue: '10',
+              index: 2,
+            ),
             BottomNavBarItem(
-                icon: AppAssets.userIcon,
-                label: AppStrings.profile,
-                showBadge: false,
-                badgeValue: ''),
+              icon: AppAssets.userIcon,
+              label: AppStrings.profile,
+              showBadge: false,
+              badgeValue: '',
+              index: 3,
+            ),
           ],
         ),
       ),
     );
+  }
+}

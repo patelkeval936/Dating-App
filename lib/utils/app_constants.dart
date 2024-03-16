3261bf74837f8a4f9b50e4c0a1a7da8e3cac0743
Keval Aamodra <patelkeval936@gmail.com>
2024-03-17 00:00:20 +0530

    updated videos links

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -5,7 +5,10 @@ class AppConstants{
   static const panUpdateAnimationDuration = 100;
 
   static const String backgroundVideoLink =
-      "https://firebasestorage.googleapis.com/v0/b/flavor-dev-b6044.appspot.com/o/from_the_air_-_9798%20(Original).mp4?alt=media&token=ddf2ed70-29ab-491f-babd-64726c42bea8";
+      "https://firebasestorage.googleapis.com/v0/b/stocky-d7891.appspot.com/o/180569%20(Original).mp4?alt=media&token=59bc20b2-db0e-41a6-9d33-710c97dc18e1";
+
+  ///4k Video Link 2
+  // "https://firebasestorage.googleapis.com/v0/b/flavor-dev-b6044.appspot.com/o/from_the_air_-_9798%20(Original).mp4?alt=media&token=ddf2ed70-29ab-491f-babd-64726c42bea8";
 
   /// use commented for HTTP Live Streaming video
   // "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8";
